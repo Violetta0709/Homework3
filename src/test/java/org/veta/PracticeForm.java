072090467b3a58d1a7e34da3249de05f2e875726
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-09-26 23:02:14 +0300

    homework3

--- src/test/java/org/veta/PracticeForm.java
+++ src/test/java/org/veta/PracticeForm.java
@@ -4,28 +4,29 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeForm {
    @BeforeAll
   static void configure () {
        Configuration.baseUrl = "https://demoqa.com";
-       Configuration.browserSize = "1920x1536";
+       Configuration.browserSize = "1920x1080";
        Configuration.holdBrowserOpen = true;
           }
 
     @Test
     void fillFormTest () {
        open("/automation-practice-form");
+       zoom(0.7);
        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
        $("#firstName").setValue("Veta");
        $("#lastName").setValue("Iuzykhovich");
        $("#userEmail").setValue("veta@veta.com");
        $("#genterWrapper").$(byText("Male")).click();
-       $("#userNumber").setValue("123456789");
+       $("#userNumber").setValue("1234567891");
        $("#dateOfBirthInput").click();
        $(".react-datepicker__month-select").selectOption("January");
        $(".react-datepicker__year-select").selectOption("2000");
@@ -34,11 +35,17 @@ public class PracticeForm {
        $("#hobbiesWrapper").$(byText("Sports")).click();
        $("#uploadPicture").uploadFromClasspath("img/7.png");
        $("#currentAddress").setValue("some address");
-       /*$("#state").click();
+       $("#state").click();
        $("#stateCity-wrapper").$(byText("NCR")).click();
        $("#city").click();
        $("#stateCity-wrapper").$(byText("Delhi")).click();
-       $("#submit").click();*/
+       $("#submit").click();
+       $(".modal-dialog").should(appear);
+       $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+       $("#table-responsive table").shouldHave(text("Iuzykhovich"), text("veta@veta.com"));
+
+
+
 
 
 
