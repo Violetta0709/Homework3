98818d4a8874643684e438042778d7175ad1fb99
Violetta Iuzykhovich <violetta100100100@gmail.com>
2022-09-29 19:05:14 +0300

    homework3

--- src/test/java/org/veta/PracticeForm.java
+++ src/test/java/org/veta/PracticeForm.java
@@ -10,49 +10,50 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeForm {
-   @BeforeAll
-  static void configure () {
-       Configuration.baseUrl = "https://demoqa.com";
-       Configuration.browserSize = "1920x1080";
-       Configuration.holdBrowserOpen = true;
-          }
-
-    @Test
-    void fillFormTest () {
-       open("/automation-practice-form");
-       zoom(0.7);
-       $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-       $("#firstName").setValue("Veta");
-       $("#lastName").setValue("Iuzykhovich");
-       $("#userEmail").setValue("veta@veta.com");
-       $("#genterWrapper").$(byText("Male")).click();
-       $("#userNumber").setValue("1234567891");
-       $("#dateOfBirthInput").click();
-       $(".react-datepicker__month-select").selectOption("January");
-       $(".react-datepicker__year-select").selectOption("2000");
-       $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
-       $("#subjectsInput").setValue("Art").pressEnter();
-       $("#hobbiesWrapper").$(byText("Sports")).click();
-       $("#uploadPicture").uploadFromClasspath("img/7.png");
-       $("#currentAddress").setValue("some address");
-       $("#state").click();
-       $("#stateCity-wrapper").$(byText("NCR")).click();
-       $("#city").click();
-       $("#stateCity-wrapper").$(byText("Delhi")).click();
-       $("#submit").click();
-       $(".modal-dialog").should(appear);
-       $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-       $("#table-responsive table").shouldHave(text("Iuzykhovich"), text("veta@veta.com"));
-
-
-
-
-
-
-
-
-
-
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
     }
 
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        zoom(0.7);
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        $("#firstName").setValue("Veta");
+        $("#lastName").setValue("Iuzykhovich");
+        $("#userEmail").setValue("veta@veta.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1234567891");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Art").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/7.png");
+        $("#currentAddress").setValue("some address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive table").shouldHave(text("Veta Iuzykhovich"),
+                text("veta@veta.com"));
+        text("Male");
+        text("1234567891");
+        text("10 January,2000");
+        text("Arts");
+        text("Sports");
+        text("7.png");
+        text("some address");
+        text("NCR Delhi");
+        /*$(".table-responsive table").$(byText("Student name"))
+                .parent().shouldHave(text("Veta Iuzykhovich"));*/
+    }
 }
